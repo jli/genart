0073216e384976e61aa39678b0f3fa1684ba4204
jli <jli@circularly.org>
2020-04-20 23:18:52 -0400

    game of life in nannou whoo

---
+++ rust-life/src/main.rs
@@ -0,0 +1,101 @@
+use std::time::Duration;
+use rand;
+use nannou::prelude::*;
+
+const GRID_SIZE: usize = 100;
+const CELL_WIDTH: usize = 10;
+
+type Grid = Vec<Vec<bool>>;
+
+struct Model {
+    grid: Grid
+}
+
+impl Model {
+    fn new(app: &App) -> Model {
+        const SIZE: u32 = (GRID_SIZE * CELL_WIDTH) as u32;
+        println!("window size will be: {}x{}", SIZE, SIZE);
+        app.new_window()
+            .size(SIZE, SIZE)
+            .view(view)
+            .build()
+            .unwrap();
+        app.set_loop_mode(LoopMode::Rate { update_interval: Duration::new(2, 0) } );
+        let mut rows = Vec::with_capacity(GRID_SIZE);
+        for _r in 0..GRID_SIZE {
+            let mut col = Vec::with_capacity(GRID_SIZE);
+            for _c in 0..GRID_SIZE {
+                // col.push((r + c) % 4 == 0);
+                col.push(rand::random());
+            }
+            rows.push(col);
+        }
+        Model { grid: rows }
+    }
+}
+
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let (ww, wh) = app.main_window().inner_size_points();
+    let xadj: f32 = ww as f32 / 2.0 - CELL_WIDTH as f32 / 2.0;
+    let yadj: f32 = wh as f32 / 2.0 - CELL_WIDTH as f32 / 2.0;
+    let draw = app.draw();
+    draw.background().color(BLACK);
+    for (r, row) in model.grid.iter().enumerate() {
+        for (c, val) in row.iter().enumerate() {
+            if *val {
+                draw.rect()
+                    .x_y((c * CELL_WIDTH) as f32 - xadj, yadj - (r * CELL_WIDTH) as f32)
+                    .w_h(CELL_WIDTH as f32, CELL_WIDTH as f32)
+                    .color(WHITE).stroke(BLACK);
+            }
+        }
+    }
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn alive(living: bool, num_neighbors: usize) -> bool {
+    match (living, num_neighbors) {
+        (true, 2..=3) => true,
+        (false, 3) => true,
+        _ => false
+    }
+}
+
+fn neighbor_positions(x: usize, max: usize) -> (usize, usize) {
+    if x == 0 { (max - 1, 1) }
+    else if x == max - 1 { (max - 2, 0) }
+    else { ( x - 1, x + 1) }
+}
+
+fn live_neighbors(grid: &Grid, r: usize, c: usize) -> usize {
+    let mut n = 0;
+    let (r_1, r1) = neighbor_positions(r, grid.len());
+    let (c_1, c1) = neighbor_positions(c, grid[0].len());
+    if grid[r_1][c_1] { n += 1 }
+    if grid[r_1][c] { n += 1 }
+    if grid[r_1][c1] { n += 1 }
+    if grid[r][c_1] { n += 1 }
+    if grid[r][c1] { n += 1 }
+    if grid[r1][c_1] { n += 1 }
+    if grid[r1][c] { n += 1 }
+    if grid[r1][c1] { n += 1 }
+    n
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    // println!("updating!");
+    let prev = model.grid.clone();
+    for (r, row) in prev.iter().enumerate() {
+        for (c, val) in row.iter().enumerate() {
+            model.grid[r][c] = alive(*val, live_neighbors(&prev, r, c));
+            // println!("grid {} {}: {} -> {}", r, c, val, model.grid[r][c]);
+        }
+    }
+}
+
+fn main() {
+    nannou::app(Model::new)
+        .update(update)
+        .run();
+}
