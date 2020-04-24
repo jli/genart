e9865f3d04b65fc1896ba3ae57a12099e986c31f
jli <jli@circularly.org>
2020-04-24 00:47:11 -0400

    mouse_noise: getting mouse position, VecDeque, basic physics

---
+++ nature-of-code/scribble/src/bin/mouse_noise.rs
@@ -0,0 +1,110 @@
+use nannou::noise::{NoiseFn, OpenSimplex};
+use nannou::prelude::*;
+use std::collections::VecDeque;
+
+const WIN_SIZE: f32 = 750.0;
+const NUM_SPRITES: usize = 4;
+const NOISE_MAG: f32 = 3.5;
+const ACC_SCALE: f32 = 0.003;
+const VEL_MAX: f32 = 5.0;
+const TRAIL_LIMIT: usize = 5;
+
+fn main() {
+  nannou::app(Model::new)
+    .update(Model::update)
+    .simple_window(view)
+    .size(WIN_SIZE as u32, WIN_SIZE as u32)
+    .run();
+}
+
+struct Model {
+  noise: OpenSimplex,
+  sprites: Vec<Sprite>,
+}
+
+#[derive(Debug)]
+struct Sprite {
+  noise_offset: Vector2,
+  pos: VecDeque<Vector2>,
+  vel: Vector2,
+  acc: Vector2,
+}
+
+fn noise_map(noise_val: f64) -> f32 {
+  map_range(noise_val as f64, 0.0, 1.0, 0.0, NOISE_MAG)
+}
+
+fn mag(v: &Vector2) -> f32 {
+  // todo: how to use this? https://docs.rs/nannou/0.13.1/nannou/prelude/trait.InnerSpace.html
+  // v.mag()
+  (v.x.powi(2) + v.y.powi(2)).sqrt()
+}
+
+impl Model {
+  fn new(_app: &App) -> Model {
+    let mut sprites = Vec::new();
+    for _ in 0..NUM_SPRITES {
+      sprites.push(Sprite::new());
+    }
+    Model {
+      noise: OpenSimplex::new(),
+      sprites,
+    }
+  }
+
+  fn update(app: &App, model: &mut Model, _update: Update) {
+    let mpos = app.mouse.position();
+    for sprite in model.sprites.iter_mut() {
+      sprite.update(&mpos, &model.noise, app.time);
+    }
+  }
+}
+
+impl Sprite {
+  fn new() -> Sprite {
+    let mut s = Sprite {
+      noise_offset: vec2(random_f32() * 10.0, random_f32() * 10.0),
+      pos: VecDeque::new(),
+      // pos: vec2(random_f32() * WIN_SIZE, random_f32() * WIN_SIZE),
+      vel: Vector2::zero(),
+      acc: Vector2::zero(),
+    };
+    s.pos.push_back(Vector2::zero());
+    s
+  }
+
+  fn update(self: &mut Sprite, mpos: &Vector2, noise: &OpenSimplex, time: f32) {
+    let pos0 = self.pos.back().unwrap();
+    let mouse_dist = *mpos - *pos0;
+    self.acc = mouse_dist * ACC_SCALE;
+    self.vel = (self.vel + self.acc).limit_magnitude(VEL_MAX);
+    let noisepos = vec2(
+      noise_map(noise.get([(self.noise_offset.x + time) as f64, 0.0])),
+      noise_map(noise.get([(self.noise_offset.y + time) as f64, 0.0])),
+    );
+    // avoiding https://github.com/rust-lang/rust/issues/59159
+    let newpos = *pos0 + self.vel + noisepos;
+    self.pos.push_back(newpos);
+    if self.pos.len() > TRAIL_LIMIT {
+      self.pos.pop_front();
+    }
+  }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+  let draw = app.draw();
+  draw.background().color(BLACK);
+  for sprite in model.sprites.iter() {
+    // todo: hacky. boils down to mouse dist...
+    let sat = map_range(mag(&sprite.acc), 0.0, WIN_SIZE * ACC_SCALE / 2.0, 0.0, 1.0);
+    for (i, pos) in sprite.pos.iter().enumerate() {
+      let alpha = map_range(i, 0, TRAIL_LIMIT - 1, 0.05, 0.5);
+      draw
+        .ellipse()
+        .xy(*pos)
+        .w_h(10.0, 10.0)
+        .hsva(0.0, sat, 1.0, alpha);
+    }
+  }
+  draw.to_frame(app, &frame).unwrap();
+}
--- nature-of-code/scribble/src/bin/noise_walk.rs
+++ nature-of-code/scribble/src/bin/noise_walk.rs
@@ -19,15 +19,17 @@ fn view(app: &App, Model { noise }: &Model, frame: Frame) {
   if frame.nth() == 0 {
     draw.background().color(WHITE);
   }
-  let x = noise.get([app.time as f64 / 3.0, 0.0]) * 600.0;
-  let y = noise.get([0.0, app.time as f64 / 3.0]) * 600.0;
+  let pos = vec2(
+    (noise.get([app.time as f64 / 3.0, 0.0]) * 600.0) as f32,
+    (noise.get([0.0, app.time as f64 / 3.0]) * 600.0) as f32,
+  );
   let w = (noise.get([app.time as f64 / 3.0, app.time as f64 / 3.0]) + 1.0) * 70.0;
   // if frame.nth() % 10 == 0 {
   //   println!("x,y: {},{}", x, y);
   // }
   draw
     .ellipse()
-    .x_y(x as f32, y as f32)
+    .xy(pos)
     .w_h(w.max(0.01) as f32, w.max(0.01) as f32)
     .hsva(0.0, 0.0, 0.0, 0.02);
   draw.to_frame(app, &frame).unwrap();
